f78301f58ef98ab854ab79b2b9d79ba97fe495ea
mirdaus <elenaabditalipova@gamil.com>
2024-04-13 17:02:48 +0000

    add class6 files

---
+++ class6-state/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-2"
+  
+}
+
+variable region {
+    default = ""
+    type = string  
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "kaizen"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
