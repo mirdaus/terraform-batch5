48fbcfbd9a7bd1c399533f697eb4f87c982c1dc5
mirdaus <elenaabditalipova@gamil.com>
2024-04-13 17:47:58 +0000

    add new 6

--- class6-state/backend.tf
+++ class6-state/backend.tf
@@ -2,6 +2,8 @@ terraform {
   backend "s3" {
     bucket = "elena-kaizen"
     key    = "ohio/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-2" 
+    dynamodb_table = "lock-state"
+
   }
-}
+  }
